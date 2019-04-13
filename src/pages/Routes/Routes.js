dc8db88204327c779524bfd1ef53d191245e6860
Matheus Alexandre <mathe.system@gmail.com>
2019-04-12 23:27:07 -0300

    Eslint

--- src/pages/Routes/Routes.js
+++ src/pages/Routes/Routes.js
@@ -4,16 +4,14 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Main from '../Main/Main';
 import Box from '../Box/Box';
 
-const MyComponent = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/box/:id" component={Box} />
-        <Redirect from="*" to="/" />
-      </Switch>
-    </BrowserRouter>
-  );
-};
+const MyComponent = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/box/:id" component={Box} />
+      <Redirect from="*" to="/" />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default MyComponent;
